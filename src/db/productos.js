233721755d752b78295d50c73c305cdace7cddf7
Nicolas Vazquez <vaznico93@hotmail.com>
2025-02-20 14:14:14 -0300

    agregue el DB y las imagenes

---
+++ src/db/productos.js
@@ -0,0 +1,51 @@
+let productos = [
+    {
+        id: 1,
+        nombre: "Lata de cerveza Amstel x unidad",
+        foto: "imgs/cerveza-amstel-lata.jpeg",
+        descripcion: "lata de cerveza Amstel de 200ml por unidad",
+        precio: 1000.00
+    },
+
+    {
+        id: 2,
+        nombre: "Lata de cerveza Grolsh",
+        foto: "imgs/cerveza-grolsh-lata.jpeg",
+        descripcion: "lata de cerveza Grolsh de 200ml por unidad",
+        precio: 1200.00
+    },
+
+    {
+        id: 3,
+        nombre: "Lata de cerveza Heineken",
+        foto: "imgs/cerveza-heineken-lata.jpeg",
+        descripcion: "lata de cerveza Heineken de 200ml por unidad",
+        precio: 1150.00   
+    },
+
+    {
+        id: 4,
+        nombre: "Lata de cerveza Schneider",
+        foto: "imgs/cerveza-schneider-lata.jpeg",
+        descripcion: "lata de cerveza Schneider de 200ml por unidad",
+        precio: 900.00    
+    },
+
+    {
+        id: 5,
+        nombre: "Combo 1: 2 Fernet Branca de 750ml + 2 Coca-Cola de 2,25ml",
+        foto: "imgs/combo-branca-coca.jpeg",
+        descripcion: "Combo 1: 2 Fernet Branca de 750ml + 2 Coca-Cola de 2,25ml",
+        precio: 20000.00    
+    },
+
+    {
+        id: 6,
+        nombre: "Combo 2: 1 Fernet Branca 750ml + 2 Coca-Cola de 2,25 ml",
+        foto: "imgs/combo-branca-cocas.jpeg",
+        descripcion: "Combo 2: 1 Fernet Branca 750ml + 2 Coca-Cola de 2,25 ml",
+        precio: 15000.00    
+    },
+]
+
+export default productos
